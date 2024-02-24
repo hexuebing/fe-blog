4af31554c054a83ddb19494ecb2c96b3158210cb
hexuebing <839308443@qq.com>
2024-02-24 11:14:51 +0000

    Deploying to gh-pages from @ hexuebing/fe-blog@9a7062a3278a4359a8eb49756c22ea2b1c7c7380 🚀

---
+++ assets/nodeInstall.html-4ezGFBKv.js
@@ -0,0 +1,14 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as e,f as a}from"./app-rgmlk_XC.js";const i={},d=a(`<h1 id="node源码安装" tabindex="-1"><a class="header-anchor" href="#node源码安装" aria-hidden="true">#</a> Node源码安装</h1><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">wget</span> https://nodejs.org/dist/v12.16.3/node-v12.16.3.tar.gz
+
+<span class="token function">tar</span> zxvf node-v12.16.3.tar.gz
+
+<span class="token builtin class-name">cd</span> node-v12.16.3
+
+./configure
+
+<span class="token function">make</span>
+
+<span class="token function">sudo</span> <span class="token function">make</span> <span class="token function">install</span>
+
+<span class="token function">node</span> —version
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),l=[d];function c(t,o){return s(),e("div",null,l)}const u=n(i,[["render",c],["__file","nodeInstall.html.vue"]]);export{u as default};
